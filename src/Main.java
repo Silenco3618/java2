f38139101de86b94cc7c4f19639789934b6c2121
Julia Silenco <silenko10031986@gmail.com>
2022-10-08 11:23:01 +0400

    change

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.net.SocketOption;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 50;
+        int bonus = amount / 20;
+        System.out.println("Бонусные мили = " + bonus);
+    }
+}
